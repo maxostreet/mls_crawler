d13450bbc3387e0be9437fd0dadf2eda16ba5dfd
James Cui <james.q.cui@gmail.com>
2015-04-01 14:04:40 -0400

    Add more fields; and refactoring

--- crawler.py
+++ crawler.py
@@ -5,12 +5,16 @@ import pprint
 import collections
 
 URL = "http://v3.torontomls.net/Live/Pages/Public/Link.aspx?Key=0c46e2aed7604e3ca10d56abf498a599&App=TREB"
-CSV_FILE = "data.csv"
+CSV_FILE = "mls_data.csv"
 
 DATA_FIELDS = {
         "mls_num" : "MLS#:",
         "seller" : "Seller:",
         "sold_price" : "Sold:",
+        "list_price" : "List:",
+        "percent_diff" : "% Dif:",
+        "dom" : "DOM:",
+        # "taxes" : "Taxes:",
         "fronting_on": "Fronting On:",
         "acreage": "Acreage:",
         "rms" : "Rms:",
@@ -28,9 +32,10 @@ DATA_FIELDS = {
 
         }
 
-def extract_report(report):
-    extracted_data = {}
 
+extracted_data = {}
+
+def get_address_info(report):
     # get address info
     street = report.find("span", "formfield")
     infos = [info.string for info in street.parent.parent.parent.find_next_sibling("div", "formgroup").find_all("span", "value")]
@@ -38,6 +43,25 @@ def extract_report(report):
     extracted_data["city"] = infos[0]
     extracted_data["province"] = infos[1]
     extracted_data["postal"] = infos[2]
+
+
+def get_taxes(report):
+    global extracted_data
+    tax_and_year = [e.string for e in report.find(text="Taxes:").parent.parent.parent.find_all("span", "value")]
+    extracted_data["taxes"] = tax_and_year[0] + " / " + tax_and_year[1]
+
+def get_building_style_and_type(report):
+    global extracted_data
+    style_and_type = [ e.string for e in report.find(text="Irreg:").parent.parent.find_all_previous("span", "value", limit=2)]
+    extracted_data['building_style'] = style_and_type[0]
+    extracted_data['building_type'] = style_and_type[1]
+
+def extract_report(report):
+    global extracted_data
+
+    get_address_info(report)
+    get_taxes(report)
+    get_building_style_and_type(report)
 
     # get all the other infos as specifed in DATA_FIELDS
     for search_key in DATA_FIELDS:
@@ -83,7 +107,7 @@ def write_to_csv(results):
 
 if __name__ == "__main__":
     html = urlopen(URL).read()
-    soup = BeautifulSoup(html, "lxml")
+    soup = BeautifulSoup(html, "html.parser")
     reports = [ report_container for report_container in soup.findAll("div", "link-item") ]
     report_len = len(reports)
     results = []
@@ -97,7 +121,7 @@ if __name__ == "__main__":
     for index, report_url in enumerate(reports):
         print index,"/",report_len,": ", report_url
         html = urlopen(report_url).read()
-        soup = BeautifulSoup(html, "lxml")
+        soup = BeautifulSoup(html, "html.parser")
         report = soup.find("div", "legacyBorder")
         data = extract_report(report)
         results.append(data)
